387236844093b3df1b26112fe77c902f7034b22d
YELENA-ZALMAYEV <elenkina73@gmail.com>
2024-07-02 19:39:17 +0300

    classwork

--- src/main/java/manager/ApplicationManager.java
+++ src/main/java/manager/ApplicationManager.java
@@ -5,15 +5,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ApplicationManager {
     WebDriver wd;
+    HelperUser helperUser;
 
 
     public void init() {
         wd = new ChromeDriver();
         wd.navigate().to("https://telranedu.web.app/");
+        helperUser =new HelperUser(wd);
     }
+
 
     public void stop() {
         wd.quit();
     }
 
+    public HelperUser getHelperUser() {
+        return helperUser;
+    }
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -5,7 +5,11 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase {
 
     @Test
-    public void test(){
+
+    public void loginSuccess(){
+        app.getHelperUser().openLoginRegistrationForm();
+        app.getHelperUser().fillLoginRegistrationForm("pap@gmail.com","@1234567Qq@");
+        app.getHelperUser().submitLogin();
 
     }
 
